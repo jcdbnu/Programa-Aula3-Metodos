3ff285bb3a8b0c3eab6cca152c12f7304fb118bd
JeanDias <jcdbnu@outlook.com>
2022-03-13 19:40:54 -0300

    Initial commit

---
+++ src/one/digitalinnovation/basecamp/Quadrilatero.java
@@ -0,0 +1,22 @@
+package one.digitalinnovation.basecamp;
+
+/**
+ * Classe de exemplo para o exercicio da Aula 3 de Metodos.
+ */
+public class Quadrilatero {
+
+    public static double area(double lado) {
+
+        return lado * lado;
+    }
+
+    public static double area(double lado1, double lado2) {
+
+        return lado1 * lado2;
+    }
+
+    public static double area(double baseMaior, double baseMenor, double altura) {
+
+        return ((baseMaior+baseMenor)*altura) / 2;
+    }
+}
